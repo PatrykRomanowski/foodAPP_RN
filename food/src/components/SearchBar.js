073385265930646dfa69c256e7d79d5f4f3b487b
PatrykRomanowski <patrykromanowski90@gmail.com>
2023-02-02 10:16:50 +0100

    update v2

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = (props) => {
+  return (
+    <View style={styles.background}>
+      <Feather style={styles.iconStyle} name="search" size={30} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={props.term}
+        onChangeText={(newTerm) => props.onTermChange(newTerm)}
+        onEndEditing={() => props.onTermSubmit()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#DDDDDD",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    marginTop: 15,
+  },
+
+  inputStyle: {
+    flex: 1,
+    fontSize: 14,
+  },
+
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
